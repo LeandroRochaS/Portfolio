54dd1da245b169b5dda680bc57635de0df5a4de0
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-09 21:42:01 -0300

    fix: edit text aboutme

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -12,23 +12,19 @@ export default function AboutMe() {
             <h1 className="about-title about-title-primary">About Me</h1>
 
             <p className="about-text">
-              Welcome to my corner of the web! I'm a self-taught web developer
-              with an unquenchable passion for technology. The journey into the
-              realm of IT began during my university days. Intrigued by the
-              possibilities, I dove headfirst into the dynamic world of web
-              development. My coding odyssey kicked off with Python, leading me
-              to explore a diverse array of languages such as Java, HTML, CSS,
-              JavaScript, React, TypeScript, Spring, and more. The thrill of
-              unraveling the intricacies of each language and framework keeps me
-              fueled on this ever-evolving adventure. Driven by an insatiable
-              curiosity, I am always on the lookout for fresh challenges and
-              opportunities to enhance my skills and broaden my knowledge. The
-              dynamic nature of technology ensures that there's always something
-              new to discover and master, and I thrive on staying at the
-              forefront of these innovations. Join me on this exciting journey
-              as I continue to push the boundaries of what's possible in the
-              digital landscape. Let's create, innovate, and build the future
-              together.
+              Welcome to my corner on the web! I'm a self-taught web developer
+              with an immense passion for technology. My journey into the world
+              of IT began during my university days. Intrigued by the
+              possibilities, I dove headfirst into the dynamic universe of web
+              development. Starting with Python, I explored a variety of
+              languages such as Java, HTML, CSS, JavaScript, React, TypeScript,
+              Spring, and more. The thrill of unraveling the intricacies of each
+              language and framework keeps me excited in this ever-evolving
+              adventure. Driven by curiosity, I'm always on the lookout for
+              challenges and opportunities to enhance my skills. Join me on this
+              exciting journey as we continue to push the boundaries of what's
+              possible in the digital landscape. Let's create, innovate, and
+              build the future together.
             </p>
           </div>
           <div className="about-work">
