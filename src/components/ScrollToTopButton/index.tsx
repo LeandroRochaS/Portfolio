7148d0e1fd068fd69daadb3fd613901f7f800429
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-09 21:40:25 -0300

    feat: detection where the button should appear

--- src/components/ScrollToTopButton/index.tsx
+++ src/components/ScrollToTopButton/index.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import "./style.scss";
 
 export default function ScrollToTopButton() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 700) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   function handleClick() {
     window.scrollTo({
       top: 0,
@@ -9,8 +28,27 @@ export default function ScrollToTopButton() {
   }
 
   return (
-    <button onClick={handleClick} className="scroll-to-top-button">
-      &#9650;
-    </button>
+    <div>
+      <svg
+        className={
+          isScrolled
+            ? "scroll-to-top-button"
+            : "scroll-to-top-button display-none"
+        }
+        onClick={handleClick}
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          d="M7 11L12 6L17 11M12 18V7"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+      </svg>
+    </div>
   );
 }
