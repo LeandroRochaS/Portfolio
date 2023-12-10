bd79166146badf66c5f4c5b51525135447606921
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-09 21:30:26 -0300

    refactor: canceled the translation idea

--- src/translate/language/index.ts
+++
@@ -1,7 +0,0 @@
-import { messages as portugueseMessages } from "./pt";
-
-const messages = {
-  ...portugueseMessages,
-};
-
-export { messages };
--- src/translate/language/pt.ts
+++
@@ -1,5 +0,0 @@
-export const messages = {
-  pt: {
-    translations: {},
-  },
-};
